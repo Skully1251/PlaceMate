2d79c154cf18a79df898250757184e8cc31138b1
Skully <2006negiom1505@gmail.com>
2026-04-15 10:50:45 +0530

    updated firebase.js

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -11,13 +11,13 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyD6rG6z_IWlEqn7FmT1y8L1VB_YwXnf2sI",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "quantix-59574.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "quantix-59574",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "quantix-59574.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "1098061150583",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:1098061150583:web:938d601d202038ccd9c972",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-GXJWW9PYQY",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
